305f9acc8253ac2c3c3984ee2e43e6a547944832
Jeremy Benoist <jeremy.benoist@gmail.com>
2019-04-04 10:26:14 +0200

    Avoid "Internal error" when no url are given
    Instead provide a clear message to the user on how the parser should be used.

--- src/mercury-parser.js
+++ src/mercury-parser.js
@@ -3,6 +3,15 @@ import Mercury from '@postlight/mercury-parser';
 import { corsSuccessResponse, corsErrorResponse, runWarm } from './utils';
 
 const mercuryParser = async ({ queryStringParameters }, context, cb) => {
+  if (!queryStringParameters) {
+    return cb(
+      null,
+      corsErrorResponse({
+        message: 'Provide an URL in the query string: ?url=',
+      })
+    );
+  }
+
   const { url } = queryStringParameters;
 
   const result = await Mercury.parse(url);
