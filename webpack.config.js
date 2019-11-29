733ed622eb636cb62726a76905465ced9dcb825f
Wajeeh Zantout <wajeeh.zantout@gmail.com>
2019-11-29 14:22:44 +0200

    chore: update node version

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,11 @@
+const nodeExternals = require('webpack-node-externals');
 const slsw = require('serverless-webpack');
 
 module.exports = {
   entry: slsw.lib.entries,
   target: 'node',
   mode: slsw.lib.webpack.isLocal ? 'development' : 'production',
+  externals: [nodeExternals()],
   module: {
     rules: [
       {
